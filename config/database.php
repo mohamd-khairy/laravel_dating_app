7d010d4da00f9ac6ce4c6bff89d800d79e44140a
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-04-11 19:41:28 +0200

    first laravel deployment to heroku

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+$DATABASE_URL=parse_url('postgres://fgxbobgrlgjyxx:ac9e5402de6332221446f746d58971a21fb4c298eeafeceef5f44f943b92ba4d@ec2-54-147-209-121.compute-1.amazonaws.com:5432/d67g27gdptnoh9');
 
 return [
 
@@ -26,7 +27,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,14 +69,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
+            'sslmode' => 'prefer',
         ],
 
     ],
